deda87b377b048814bc4ec75fd7a85ec8c723c8c
PolinaZinovieva <94112713+PolinaZinovieva@users.noreply.github.com>
2023-04-02 16:12:27 +0200

    -homework 9

--- Zinovieva_hw10.py
+++ Zinovieva_hw10.py
@@ -9,26 +9,35 @@ def exc(file_name):
         integer = int(lines.strip())
     except AttributeError:
         print("Attribute error!")
+
+
 exc("file.txt")
 
 # Task 3
 
-def date(a,b,c,d):
 
+def date(a, b, c, d):
     date1 = datetime.date(year=a, month=b, day=c)
     print(date1)
     date2 = datetime.timedelta(days=d)
     res = date1 + date2
     print(res)
-date(2022,4,13,10)
+
+
+date(2022, 4, 13, 10)
+
 
 # Task 4
-def age(a,b,c):
+def age(a, b, c):
     now = datetime.datetime.now()
     birth_date = datetime.datetime(year=a, month=b, day=c)
     age_date = now - birth_date
-    age_stamp = datetime.datetime.timestamp(now) - datetime.datetime.timestamp(birth_date)
+    age_stamp = datetime.datetime.timestamp(now) - datetime.datetime.timestamp(
+        birth_date
+    )
 
     print(f" This is datetime: {age_date}")
     print(f" This is timestamp: {age_stamp}")
-age(2001,5,21)
+
+
+age(2001, 5, 21)
