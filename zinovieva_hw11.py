cd095e1f9df66ea40cd3f1d4c4235e6f4707d549
PolinaZinovieva <94112713+PolinaZinovieva@users.noreply.github.com>
2023-04-16 18:18:11 +0200

    - hw 11 task 2

--- zinovieva_hw11.py
+++ zinovieva_hw11.py
@@ -1,3 +1,5 @@
+# Task 1
+
 import csv
 
 # Step 1 read the file and return the list to use it later
@@ -47,3 +49,17 @@ print(f"This is appended list {file}")
 with open("another.csv", mode="w") as another_csv:
     write = csv.writer(another_csv, delimiter=",")
     write.writerows(file)
+
+
+# Task 2
+
+def sq():
+    counter = 0
+    while counter < 100000:
+        yield counter ** 2
+        counter += 1
+s = sq()
+for i in s:
+    print(i)
+
+
