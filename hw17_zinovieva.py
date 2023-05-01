b9fc02e6f8f8ccb2e002a3149317a2b199b8e3d9
PolinaZinovieva <94112713+PolinaZinovieva@users.noreply.github.com>
2023-05-01 17:41:07 +0200

    - Task 1

---
+++ hw17_zinovieva.py
@@ -0,0 +1,31 @@
+# Task1
+
+def singleton(_class):
+    def inner(*args):
+        if not hasattr(_class, 'instance'):
+            setattr(_class, 'instance', _class(*args))
+        return getattr(_class, 'instance')
+    return inner
+
+
+@singleton
+class Student:
+    def __init__(self,avg_grade:float, subject:str, name: str):
+        self.__grade = avg_grade
+        self.subject = subject
+        self.creds = name
+
+    def Logic(self):
+        if self.__grade < 60:
+            print(f"{self.__grade} is not good,{self.creds} Learn {self.subject} more")
+        else:
+            print ("Ok,good")
+
+
+
+Student1 = Student(88.89, "Math", "Mariia")
+Student2 = Student(10.0, "English", " Andrew")
+print()
+
+# Task 2
+
