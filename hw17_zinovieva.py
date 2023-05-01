57bd4033b032da14a89970c41201ebb414f252f8
PolinaZinovieva <94112713+PolinaZinovieva@users.noreply.github.com>
2023-05-01 17:42:12 +0200

    - Task 1

--- hw17_zinovieva.py
+++ hw17_zinovieva.py
@@ -1,16 +1,18 @@
 # Task1
 
+
 def singleton(_class):
     def inner(*args):
-        if not hasattr(_class, 'instance'):
-            setattr(_class, 'instance', _class(*args))
-        return getattr(_class, 'instance')
+        if not hasattr(_class, "instance"):
+            setattr(_class, "instance", _class(*args))
+        return getattr(_class, "instance")
+
     return inner
 
 
 @singleton
 class Student:
-    def __init__(self,avg_grade:float, subject:str, name: str):
+    def __init__(self, avg_grade: float, subject: str, name: str):
         self.__grade = avg_grade
         self.subject = subject
         self.creds = name
@@ -19,13 +21,9 @@ class Student:
         if self.__grade < 60:
             print(f"{self.__grade} is not good,{self.creds} Learn {self.subject} more")
         else:
-            print ("Ok,good")
-
+            print("Ok,good")
 
 
 Student1 = Student(88.89, "Math", "Mariia")
 Student2 = Student(10.0, "English", " Andrew")
 print()
-
-# Task 2
-
