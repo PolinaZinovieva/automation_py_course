8ae578976b49f23846fe69560902ca929d67c89f
PolinaZinovieva <94112713+PolinaZinovieva@users.noreply.github.com>
2023-06-12 20:47:27 +0200

    report

--- lesson31/models/users.py
+++ lesson31/models/users.py
@@ -7,8 +7,10 @@ class Users(Base):
     __tablename__ ='usersc'
     id = Column(VARCHAR(8),primary_key=True)
     first_name = Column(VARCHAR(50))
-    last_name = Column(VARCHAR(50))
     age = Column(Integer)
     email = Column(VARCHAR(50))
+
+    def __str__(self):
+        return f" id = {self.id}, first_name = {self.first_name}, age = {self.age}, email = {self.email}"
 
 
--- lesson31/repos/users_repos.py
+++ lesson31/repos/users_repos.py
@@ -9,5 +9,29 @@ class Users_Repository:
     def get_all(self):
         for user in self.__session.query(Users).all():
             print(user)
+
+    def insert_one(self, user: Users):
+        self.__session.add(user)
+        self.__session.commit()
+
+    def one_by_email(self, email):
+        print(self.__session.query(Users).filter_by(email=email).first())
+
+
+
+
+
+
+
+
+
+
+
+
+
 users_repos = Users_Repository()
 users_repos.get_all()
+# users_repos.insert_one(Users(id = '2', first_name = ' Jack', age ='2', email = 'jack"gmail.com'))
+# users_repos.get_all()
+
+users_repos.one_by_email(email='a@gmail')
